a70c375c0505308d910b8f1d50e91b857195bdc9
Sara Isabel del Sastre <chinchimilla@gmail.com>
2026-03-27 22:51:45 +0100

    intentando conectar con front

--- app.js
+++ app.js
@@ -1,7 +1,12 @@
 require("dotenv").config();
 
 const express = require('express');
+const cors    = require('cors');
 const app     = express();
+
+app.use(cors({
+    origin: process.env.FRONTEND_URL
+}));
 
 app.get('/', (req,res) => {
     res.send('Hola Mundo!!')
--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const { dbConnection } = require('./config/db')
 const app  = require('./app')
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
+
 
 dbConnection();
 
