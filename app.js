b10d08b4ade6daef69d36bb8033019d2c6c4c607
Sara Isabel del Sastre <chinchimilla@gmail.com>
2026-03-29 00:05:36 +0100

    sigo intentando conectar back con front

--- app.js
+++ app.js
@@ -4,9 +4,16 @@ const express = require('express');
 const cors    = require('cors');
 const app     = express();
 
+//app.use(cors({
+  //  origin: process.env.FRONTEND_URL
+//}));
+//app.use(cors());
 app.use(cors({
-    origin: process.env.FRONTEND_URL
+  origin: "https://animaladas.netlify.app",
+  credentials: true
 }));
+
+app.options('*', cors());
 
 app.get('/', (req,res) => {
     res.send('Hola Mundo!!')
