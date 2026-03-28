0dcc8fd635a14a745066a77f556fe8fd69d9c32e
Sara Isabel del Sastre <chinchimilla@gmail.com>
2026-03-29 00:32:32 +0100

    fix cors

--- app.js
+++ app.js
@@ -4,15 +4,13 @@ const express = require('express');
 const cors    = require('cors');
 const app     = express();
 
-// Define las opciones de CORS UNA VEZ
 const corsOptions = {
   origin: "https://animaladas.netlify.app",
   credentials: true
 };
 
-// Aplica CORS con las mismas opciones en ambos lugares
 app.use(cors(corsOptions));
-app.options('*', cors(corsOptions));  // <-- AHORA USA LAS MISMAS OPCIONES
+// NO USES app.options('*', ...) - ya no funciona en Express 5
 
 app.get('/', (req, res) => {
     res.send('Hola Mundo!!')
