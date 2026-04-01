07dc8d8fbde837a66b52adf3eb81efcf32e6a8b0
Sara Isabel del Sastre <chinchimilla@gmail.com>
2026-04-01 11:23:11 +0200

    vuelvo a modificar CORS

--- app.js
+++ app.js
@@ -10,9 +10,7 @@ const postRoutes    = require('./routes/postRoutes');
 const authRoutes    = require('./routes/authRoutes');
 
 const corsOptions = {
-  origin: "https://animaladas.netlify.app",
-  origin: "http://localhost:5174",
-  origin: "http://localhost:5173",
+  origin: ['https://animaladas.netlify.app', 'http://localhost:5174', 'http://localhost:5173'],
   credentials: true
 };
 
